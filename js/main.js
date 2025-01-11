4c544005b05770a0478a725c3d8e584f39e095db
George Konovalov <george.konovalov@gmail.com>
2025-01-11 18:40:29 +0200

    Add user msg if input less than 3 chars or if not ending with question symbol

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
 'use strict'
 
+function onAsk(ev) {
+    ev.preventDefault()
+    checkInput()    
+}
+
+function checkInput() {
+    const userInput = document.querySelector('.user-question').value
+    console.log('user input:', userInput)
+    
+    if (userInput.length < 3 || userInput.slice(-1) !== '?') {
+        showUserMsg()
+        return
+    }
+}
+
+function showUserMsg() {
+    document.querySelector('.user-msg').hidden = false
+}
+
+function hideUserMsg() {
+    document.querySelector('.user-msg').hidden = true
+}
